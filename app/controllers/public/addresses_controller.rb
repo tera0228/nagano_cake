05666bd2318cb1631315569d686a0f51849ee488
tera0228 <temple0228@gmail.com>
2022-05-16 13:58:29 +0000

    配送画面、機能追加

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,47 @@
 class Public::AddressesController < ApplicationController
+  def index
+    @address = Address.new
+    @addresses = Address.all
+  end
+
+  def edit
+    @address = Address.find(params[:id])
+  end
+
+  def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+       flash[:notice] = "配送先を登録しました"
+       redirect_to addresses_path(@address.customer_id)
+    else
+       flash[:notice] = "必要情報を入力してください/ハイフンは使用できない"
+       redirect_to addresses_path
+    end
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    if @address.customer_id = current_customer.id
+       @address.update(address_params)
+       flash[:notice] = "配送先を編集しました"
+       redirect_to addresses_path
+    else
+      flash[:notice] = "必要情報を入力してください/ハイフンは使用できない"
+      render :edit
+    end
+  end
+
+  def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    flash[:notice] = "配送先を削除しました"
+    redirect_to addresses_path
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:customer_id, :name, :postal_code, :address)
+  end
 end
