50e7ba094dc976b30f8d7e9ff655b7f7f958ecba
tera0228 <temple0228@gmail.com>
2022-05-19 13:54:25 +0000

    会員サイト作成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,20 @@
 class Public::CartItemsController < ApplicationController
+  def index
+    @cart_items = Cart_item.all
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def create
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :customer_id, :amount)
+  end
 end
